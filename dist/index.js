a8e50bd3d9f0500199c7e5e4e5bfb5c295a99599
BUDISANTOSO <budi.santoso@salt.co.id>
2024-02-12 10:55:31 +0700

    Adjust package.json

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ exports.createGreetingBuilder = void 0;
 const architect_1 = require("@angular-devkit/architect");
 const rxjs_1 = require("rxjs");
 const createGreetingBuilder = (options, context) => {
-    context.logger.info("Hello from Greeting Builder", options);
+    context.logger.info(`Hello from Greeting Builder | ${JSON.stringify(options)}`, options);
     return (0, rxjs_1.of)({ success: true });
 };
 exports.createGreetingBuilder = createGreetingBuilder;
